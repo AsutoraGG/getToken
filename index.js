f48e302c0da7605208ee8a5472a97b61ac25a96d
AsutoraGG <76235964+AsutoraGG@users.noreply.github.com>
2022-09-02 22:52:05 +0900

    🥳
    Relese!

--- index.js
+++ index.js
@@ -13,28 +13,28 @@ if(existsSync('rustplus.config.json')) { /* rustplus.config.jsonがあるか確�
     listen(config.fcm_credentials, ({ notification, persistentId }) => {
         const body = JSON.parse(notification.data.body);
 
-        var date = {
+        var date = { // Save Data
             "IP": body.ip,
             "PORT": body.port,
             "ID": body.playerId,
             "TOKEN": body.playerToken
         }
 
-        if(settings.Loadall === true) {
-            writeFile('./config.json', JSON.stringify(date, null, 2)); // JSONで保存
+        if(settings.Loadall === true) { // ALL Message
+            writeFile('./config.json', JSON.stringify(date, null, 2)); // Save Json
             console.log(`IP : "${body.ip}" \nPort : "${body.port}" \nSteamID : "${body.playerId}" \nPlayerToken : "${body.playerToken}"`);
             console.log(`ServerName : "${body.name}" \n ServerDescription: "${body.desc}"`);
             if(settings.dev === true) return console.log(`Type: ${body.type}`);
             console.log('[INFO] : Saved config.json');
         }
-        else {
+        else { // IP,PORT,ID,TOKEN Only(type)
             writeFile('./config.json', JSON.stringify(date, null, 2)); // JSONで保存
             console.log(`IP : "${body.ip}" \nPort : "${body.port}" \nSteamID : "${body.playerId}" \nPlayerToken : "${body.playerToken}"`);
             if(settings.dev === true) return console.log(`Type: ${body.type}`);
             console.log('[INFO] : Saved config.json');
         }
     });
-} else {
+} else { // if not found rustplus.config.json
     console.log('[Error] : rustplus.config.json is Not found!');
     console.log('[Error] : Run "npx @liamcottle/rustplus.js fcm-register" ');
     process.exit(0);
