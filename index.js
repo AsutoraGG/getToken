cc0395201ad61ef73e1e941c5bb9285846949c3b
AsutoraGG <76235964+AsutoraGG@users.noreply.github.com>
2022-09-02 17:09:14 +0900

    Update
    データをjsonとしてセーブできるように更新
    まだ試していないのでエラーが起これば修正

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-/* --- Made by @AsutoraGG --- */
-const { register, listen } = require('push-receiver');
-const { existsSync } = require('fs');
+/* Made by @AsutoraGG */
+const { listen } = require('push-receiver');
+const { existsSync, writeFile } = require('fs');
 
+const settings = require('./settings.json');
 const config = require('./rustplus.config.json');
 
-if(existsSync('rustplus.config.json')) { /* rustplus.config.jsonがあるか確認 */
-    console.clear(); //コンソールをクリア
+if(existsSync('rustplus.config.json')) { /* rustplus.config.jsonがあるか確認。なかったらelse */
+    console.clear();
     console.log("[INFO] : found rustplus.config.json!");
 
-    listen(config.fcm_credentials, ({ notification, persistentId }) => { /* Rust+とのペアリングがあった時にお知らせ */
-        const body = JSON.parse(notification.data.body); /* お知らせの内容を取得 */
-        console.log(`IP : "${body.ip}" \nPort : "${body.port}" \nSteamID : "${body.playerId}" \nPlayerToken : "${body.playerToken}"`); /* IP Port ID Tokenのみ表示 */
+    listen(config.fcm_credentials, ({ notification, persistentId }) => {
+        const body = JSON.parse(notification.data.body);
+
+        var date = {
+            "IP": body.ip,
+            "PORT": body.port,
+            "ID": body.id,
+            "TOKEN": body.token
+        }
+
+        if(settings.Loadall === true) {
+            writeFile('./config.json', JSON.stringify(date, null, 2)); // JSONで保存
+            console.log(`IP : "${body.ip}" \nPort : "${body.port}" \nSteamID : "${body.playerId}" \nPlayerToken : "${body.playerToken}"`);
+            console.log(`ServerName : "${body.name}" \n ServerDescription: "${body.desc}"`);
+            if(settings.dev === true) return console.log(`Type: ${body.type}`);
+            console.log('[INFO] : Saved config.json');
+        }
+        else {
+            writeFile('./config.json', JSON.stringify(date, null, 2)); // JSONで保存
+            console.log(`IP : "${body.ip}" \nPort : "${body.port}" \nSteamID : "${body.playerId}" \nPlayerToken : "${body.playerToken}"`);
+            if(settings.dev === true) return console.log(`Type: ${body.type}`);
+            console.log('[INFO] : Saved config.json');
+        }
     });
-} else { /* もし .jsonがなかった場合プロセスを終了 */
+} else {
     console.log('[Error] : rustplus.config.json is Not found!');
-    console.log('[Error] : Run the "npx @liamcottle/rustplus.js fcm-register" ');
+    console.log('[Error] : Run "npx @liamcottle/rustplus.js fcm-register" ');
     process.exit(0);
 };
